32fe35b829eb2030de9341c8eb121f8ee02f2865
George W F Santos <gweverton21@hotmail.com>
2020-06-05 17:56:43 -0300

    add image upload feature

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -11,6 +11,8 @@ import "./styles.css";
 
 import logo from "../../assets/logo.svg";
 
+import Dropzone from "../../components/Dropzone";
+
 interface Item {
   id: number;
   title: string;
@@ -48,6 +50,8 @@ const CreatePoint: React.FC = () => {
     0,
     0,
   ]);
+  const [selectedFile, setSelectedFile] = useState<File>();
+
   const history = useHistory();
 
   useEffect(() => {
@@ -145,16 +149,20 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItemIds;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     await api.post("points", data);
 
@@ -179,6 +187,8 @@ const CreatePoint: React.FC = () => {
           Cadastro do <br />
           ponto de coleta
         </h1>
+
+        <Dropzone onFileUpload={setSelectedFile} />
 
         <fieldset>
           <legend>
