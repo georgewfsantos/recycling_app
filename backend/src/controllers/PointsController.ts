f1c7cbfe36de924624a6ef7594d279d29f35c9f3
George W F Santos <gweverton21@hotmail.com>
2020-06-05 17:54:18 -0300

    add image upload feature

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -17,7 +17,14 @@ class PointsController {
       .distinct()
       .select("points.*");
 
-    return response.json(points);
+    const serializedPoints = points.map((point) => {
+      return {
+        ...point,
+        image_url: `http://192.168.0.107:3333/uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
   }
 
   async create(request: Request, response: Response) {
@@ -35,8 +42,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image:
-        "https://images.unsplash.com/photo-1512415031623-5c8392938dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -50,12 +56,15 @@ class PointsController {
 
     const point_id = insertedIds[0];
 
-    const pointToItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      };
-    });
+    const pointToItems = items
+      .split(",")
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
+      });
 
     await trx("point_to_items").insert(pointToItems);
 
@@ -76,12 +85,17 @@ class PointsController {
       return response.status(400).json({ message: "Point not found" });
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.0.107:3333/uploads/${point.image}`,
+    };
+
     const items = await knex("items")
       .join("point_to_items", "items.id", "=", "point_to_items.item_id")
       .where("point_to_items.point_id", id)
       .select("items.title");
 
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
   }
 }
 
